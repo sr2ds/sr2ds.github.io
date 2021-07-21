a1d75275f25e59405df24b63a56832e06e256887
David Silva <david.silva@processmind.com.br>
2021-07-21 14:46:34 -0300

    env path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "./docs")
+  outputDir: path.resolve(__dirname, "./docs"),
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/docs/'
+  : '/'
 }
